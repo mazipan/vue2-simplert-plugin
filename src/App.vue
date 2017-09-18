e14c544f5dc798d92626b846e180574d374f336f
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-09-18 18:44:41 +0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+</style>

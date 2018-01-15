dedba6dd35bd0cd10714be5f020f284549c13ced
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-01-15 16:40:41 +0700

    delete js file

--- src/Simplert.vue
+++ src/Simplert.vue
@@ -1,6 +1,26 @@
-<template src="vue2-simplert-core/simplert.html"></template>
+<template
+  src="vue2-simplert-core/simplert.html">
+</template>
 
-<style src="vue2-simplert-core/simplert.css"></style>
+<script>
+import simplertMixin from 'vue2-simplert-core'
+import { SimplertEventBus } from './simplert-event-bus.js'
 
-<script src="./simplert.js"></script>
+export default {
+  name: 'Simplert',
+  mixins: [
+    simplertMixin
+  ],
+  created () {
+    // listen eventBus
+    SimplertEventBus.$on('open', this.openSimplert);
+    SimplertEventBus.$on('close', this.justCloseSimplert);
+  }
+}
+
+</script>
+
+<style
+  src="vue2-simplert-core/simplert.css">
+</style>
 

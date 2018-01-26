79700b088c9818be05037cf8eda4e66437e75d49
guansunyata <guansunyata@gmail.com>
2018-01-26 16:25:56 +0800

    [feature] add customize default config

--- src/simplert-plugin.js
+++ src/simplert-plugin.js
@@ -2,18 +2,22 @@ import SimplertComponent from './Simplert.vue'
 import { SimplertEventBus } from './simplert-event-bus.js'
 
 const Simplert = {
-  install (Vue = {}) {
+  install (Vue = {}, options = {}) {
 
     let SimplertPlugin = {
       $vm: null,
       state: {},
-      init(vm) {
+      data: {
+        options: {}
+      },
+      init(vm, opts) {
         this.$vm = vm
+        this.options = opts;
       },
       open(obj) {
         if (!this.$vm) return
         obj.show = true
-        this.$vm.$emit('open', obj);
+        this.$vm.$emit('open', Object.assign({}, this.options, obj));
       },
       close() {
         if (!this.$vm) return
@@ -21,7 +25,7 @@ const Simplert = {
       }
     }
 
-    SimplertPlugin.init(SimplertEventBus)
+    SimplertPlugin.init(SimplertEventBus, options)
 
     Vue.component('simplert', SimplertComponent)
     Vue.prototype.$Simplert = SimplertPlugin

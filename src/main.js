98765e518fc2fcb819043d1a05f602b794420a38
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-04-24 10:49:49 +0700

    add highlightjs

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueHighlightJS from 'vue-highlightjs'
 
 /* global process */
 if (process.env.NODE_ENV !== 'production') {
   Vue.config.devtools = true
 }
 
+Vue.use(VueHighlightJS)
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: {App}
+  components: {App},
+  template: '<App/>'
 })

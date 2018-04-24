38651805edab4810945e961507b81af560495308
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-04-24 10:54:47 +0700

    fix demo highlight-js

--- src/demo.js
+++ src/demo.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
+import VueHighlightJS from 'vue-highlightjs'
 import App from './App.vue'
+import Simplert from './simplert-plugin.js'
 
 /* global process */
 if (process.env.NODE_ENV !== 'production') {
   Vue.config.devtools = true
 }
 
-import Simplert from './simplert-plugin.js'
 Vue.use(Simplert, {})
+Vue.use(VueHighlightJS)
 
 new Vue({
   el: '#app',
